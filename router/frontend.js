85a3b3c96a825da228c35e155d17d6cff0e52227
lalit <lalitvisionvivante@gmail.com>
2022-08-09 11:50:37 +0530

    add-product-functionality

--- router/frontend.js
+++ router/frontend.js
@@ -4,6 +4,7 @@ const auth = require("../middleware/auth");
 
 const router = new express.Router();
 var userscontroller = require('../controllers/frontend/userscontroller');
+var productcontroller = require('../controllers/frontend/productcontroller');
 var update =  async function(req,res){
     var data= [1,2,3];
     return res.status(200).json({ "status": true,"data": data });
@@ -17,5 +18,6 @@ router.get("/user", function(req,res){
 });*/
 
 router.post("/user_create", userscontroller.create);
+router.post("/add_product", productcontroller.product_create);
 
 module.exports = router;
